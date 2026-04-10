28c0e18e95f8aeb26a620f6058ed63014129aefa
Alexander Popel <alexander.popel@postindustria.com>
2026-04-10 11:18:37 +0300

    ci: switch package-update to dotnet-outdated

--- ci/update-packages.ps1
+++ ci/update-packages.ps1
@@ -1,15 +1,3 @@
+param([Parameter(Mandatory)][string]$RepoName)
 
-param(
-    [string]$ProjectDir = ".",
-    [string]$Name,
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName
-)
-
-./dotnet/run-update-dependencies.ps1 `
-    -IncludePrerelease `
-    -RepoName $RepoName `
-    -ProjectDir $ProjectDir `
-    -Name $Name
-
-exit $LASTEXITCODE
+./dotnet/outdated.ps1 -RepoName:$RepoName
